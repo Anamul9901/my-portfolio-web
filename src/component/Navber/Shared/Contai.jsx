1acb6826cb5ba29eb66107e4fed46e556b381e88
Anamul9901 <anamulhaque9901@gmail.com>
2023-12-11 09:07:08 +0600

    setup

---
+++ src/component/Navber/Shared/Contai.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+ 
+
+const Contai = ({children}) => {
+    return (
+        <div className="max-w-[1250px] w-full mx-auto xl:px-20 sm:px-2 px-4">
+            {children}
+        </div>
+    );
+};
+
+export default Contai;
